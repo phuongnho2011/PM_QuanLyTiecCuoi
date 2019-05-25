9c58b9ee84f6d4e6250376e95f59d192f76af399
Tran Hoang Phuong <phuongnho296@gmail.com>
2019-05-25 12:22:53 +0700

    new version

---
+++ WeddingApp/WeddingApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WeddingApp.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public ICommand FoodCommand { get; set; }
+        public MainViewModel()
+        {
+            FoodCommand = new RelayCommand<object>((p) => { return true; }, (p) => { FoodWindow wd = new FoodWindow(); wd.ShowDialog(); });
+        }
+    }
+}
